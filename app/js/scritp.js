ab1e6b31afcc4620e6b8f5e1dcf09b26dab19014
Saqlainkhadim <Saqlainkhadimbusiness@gmail.com>
2023-06-17 22:58:48 +0500

    Animated header

--- app/js/scritp.js
+++ app/js/scritp.js
@@ -1,1 +1,15 @@
-console.log('Hello world!');
+const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+
+btnHamburger.addEventListener("click", function () {
+    if (header.classList.contains("open")) {
+        header.classList.remove("open");
+        overlay.classList.remove("fadeout");
+        overlay.classList.add("fadein");
+    } else {
+        header.classList.add("open");
+        overlay.classList.add("fadeout");
+        overlay.classList.remove("fadein");
+    }
+});
