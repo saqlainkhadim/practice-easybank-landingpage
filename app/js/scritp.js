b99be169ec53fb8005fa3a50a4e8a7af888cde5f
Saqlainkhadim <Saqlainkhadimbusiness@gmail.com>
2023-06-18 02:12:07 +0500

    nav menu complete

--- app/js/scritp.js
+++ app/js/scritp.js
@@ -1,15 +1,25 @@
 const btnHamburger = document.querySelector("#btnHamburger");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
+const body = document.querySelector("body");
+const fadeElems = document.querySelectorAll(".hasfade");
 
 btnHamburger.addEventListener("click", function () {
-    if (header.classList.contains("open")) {
+    if (header.classList.contains("open")) {// close menu
         header.classList.remove("open");
-        overlay.classList.remove("fadeout");
-        overlay.classList.add("fadein");
-    } else {
+        body.classList.remove("noscroll");
+
+        fadeElems.forEach(function (element) {
+            element.classList.remove("fadeout");
+            element.classList.add("fadein");
+        });
+        
+    } else {// open menu
         header.classList.add("open");
-        overlay.classList.add("fadeout");
-        overlay.classList.remove("fadein");
+        fadeElems.forEach(function (element) {
+            element.classList.add("fadeout");
+            element.classList.remove("fadein");
+        });
+        body.classList.add("noscroll");
     }
 });
